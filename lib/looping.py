c1c5bd284dcd75d3e2f365d4b6d180adf8166756
tray <onesimus.githua@student.moringaschool.com>
2023-11-21 14:17:45 +0300

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+
+# Call the function
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return list(map(lambda num: num ** 2, int_list))
+
+# Example usage
+numbers = [1, 2, 3, 4, 5]
+result = square_integers(numbers)
+print(result)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+# Call the function
+fizzbuzz()
+
